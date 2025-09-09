c4548b43b445dd3a8420f61fa4df9f011521440d
LukasJacobWyatt <165064223+MathForSchool@users.noreply.github.com>
2025-09-09 13:40:39 -0400

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,124 +1,47 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import * as Actions from "../../actions";
-import { getTreeValue } from "../../actions";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Icon } from "../../utils/general";
-import Battery from "../shared/Battery";
-import "./searchpane.scss";
-import "./sidepane.scss";
-import "./startmenu.scss";
+import "./menu.scss";
 
-export * from "./start";
-export * from "./widget";
+import * as Actions from "../../actions";
 
-export const DesktopApp = () => {
-  const deskApps = useSelector((state) => {
-    var arr = { ...state.desktop };
-    var tmpApps = [...arr.apps];
+export const ActMenu = () => {
+  const menu = useSelector((state) => state.menus);
+  const menudata = menu.data[menu.opts];
+  const { abpos, isLeft } = useSelector((state) => {
+    var acount = state.menus.menus[state.menus.opts].length;
+    var tmpos = {
+        top: state.menus.top,
+        left: state.menus.left,
+      },
+      tmpleft = false;
 
-    if (arr.sort == "name") {
-      tmpApps.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
-    } else if (arr.sort == "size") {
-      tmpApps.sort((a, b) => {
-        var anm = a.name,
-          bnm = b.name;
+    var wnwidth = window.innerWidth,
+      wnheight = window.innerHeight;
 
-        return anm[bnm.charCodeAt(0) % anm.length] >
-          bnm[anm.charCodeAt(0) % bnm.length]
-          ? 1
-          : -1;
-      });
-    } else if (arr.sort == "date") {
-      tmpApps.sort((a, b) => {
-        var anm = a.name,
-          bnm = b.name;
-        var anml = anm.length,
-          bnml = bnm.length;
+    var ewidth = 312,
+      eheight = acount * 28;
 
-        return anm[(bnml * 13) % anm.length] > bnm[(anml * 17) % bnm.length]
-          ? 1
-          : -1;
-      });
+    tmpleft = wnwidth - tmpos.left > 504;
+    if (wnwidth - tmpos.left < ewidth) {
+      tmpos.left = wnwidth - ewidth;
     }
 
-    arr.apps = tmpApps;
-    return arr;
+    if (wnheight - tmpos.top < eheight) {
+      tmpos.bottom = wnheight - tmpos.top;
+      tmpos.top = null;
+    }
+
+    return {
+      abpos: tmpos,
+      isLeft: tmpleft,
+    };
   });
+
   const dispatch = useDispatch();
-
-  return (
-    <div className="desktopCont">
-      {!deskApps.hide &&
-        deskApps.apps.map((app, i) => {
-          return (
-            // to allow it to be focusable (:focus)
-            <div key={i} className="dskApp" tabIndex={0}>
-              <Icon
-                click={app.action}
-                className="dskIcon prtclk"
-                src={app.icon}
-                payload={app.payload || "full"}
-                pr
-                width={Math.round(deskApps.size * 36)}
-                menu="app"
-              />
-              <div className="appName">{app.name}</div>
-            </div>
-          );
-        })}
-    </div>
-  );
-};
-
-export const BandPane = () => {
-  const sidepane = useSelector((state) => state.sidepane);
-
-  return (
-    <div
-      className="bandpane dpShad"
-      data-hide={sidepane.banhide}
-      style={{ "--prefix": "BAND" }}
-    >
-      <div className="bandContainer">
-        <Icon
-          className="hvlight"
-          width={17}
-          click="CALCUAPP"
-          payload="togg"
-          open="true"
-          src="calculator"
-        />
-        <Icon
-          className="hvlight"
-          width={17}
-          click="SPOTIFY"
-          payload="togg"
-          open="true"
-          src="spotify"
-        />
-        <Icon
-          className="hvlight"
-          width={17}
-          click="NOTEPAD"
-          payload="togg"
-          src="notepad"
-        />
-      </div>
-    </div>
-  );
-};
-
-export const SidePane = () => {
-  const sidepane = useSelector((state) => state.sidepane);
-  const setting = useSelector((state) => state.setting);
-  const tasks = useSelector((state) => state.taskbar);
-  const [pnstates, setPnstate] = useState([]);
-  const dispatch = useDispatch();
-
-  let [btlevel, setBtLevel] = useState("");
-  const childToParent = () => {};
 
   const clickDispatch = (event) => {
+    event.stopPropagation();
     var action = {
       type: event.target.dataset.action,
       payload: event.target.dataset.payload,
@@ -126,193 +49,100 @@ export const SidePane = () => {
 
     if (action.type) {
       if (action.type != action.type.toUpperCase()) {
-        Actions[action.type](action.payload);
-      } else dispatch(action);
-    }
-    // For battery saver
-    if (action.payload === "system.power.saver.state") setBrightness();
-  };
-
-  const vSlider = document.querySelector(".vSlider");
-  const bSlider = document.querySelector(".bSlider");
-
-  const setVolume = (e) => {
-    var aud = 3;
-    if (e.target.value < 70) aud = 2;
-    if (e.target.value < 30) aud = 1;
-    if (e.target.value == 0) aud = 0;
-
-    dispatch({ type: "TASKAUDO", payload: aud });
-
-    sliderBackground(vSlider, e.target.value);
-  };
-
-  function sliderBackground(elem, e) {
-    elem.style.setProperty(
-      "--track-color",
-      `linear-gradient(90deg, var(--clrPrm) ${e - 3}%, #888888 ${e}%)`,
-    );
-  }
-
-  const setBrightness = (e) => {
-    var brgt = document.getElementById("brightnessSlider").value;
-    if (!e) {
-      // Battery saver
-      const state = setting.system.power.saver.state;
-      const factor = state ? 0.7 : 100 / 70;
-      const newBrgt = brgt * factor;
-      setBrightnessValue(newBrgt);
-      document.getElementById("brightnessSlider").value = newBrgt;
-    } else {
-      // Brightness slider
-      setBrightnessValue(brgt);
+        Actions[action.type](action.payload, menu);
+      } else {
+        dispatch(action);
+      }
+      dispatch({ type: "MENUHIDE" });
     }
   };
 
-  function setBrightnessValue(brgt) {
-    document.getElementById("brightoverlay").style.opacity = (100 - brgt) / 100;
-    dispatch({
-      type: "STNGSETV",
-      payload: {
-        path: "system.display.brightness",
-        value: brgt,
-      },
-    });
-    sliderBackground(bSlider, brgt);
-  }
-
-  useEffect(() => {
-    sidepane.quicks.map((item, i) => {
-      if (item.src == "nightlight") {
-        if (pnstates[i]) document.body.dataset.sepia = true;
-        else document.body.dataset.sepia = false;
+  const menuobj = (data) => {
+    var mnode = [];
+    data.map((opt, i) => {
+      if (opt.type == "hr") {
+        mnode.push(<div key={i} className="menuhr"></div>);
+      } else {
+        mnode.push(
+          <div
+            key={i}
+            className="menuopt"
+            data-dsb={opt.dsb}
+            onClick={clickDispatch}
+            data-action={opt.action}
+            data-payload={opt.payload}
+          >
+            {menudata.ispace != false ? (
+              <div className="spcont">
+                {opt.icon && opt.type == "svg" ? (
+                  <Icon icon={opt.icon} width={16} />
+                ) : null}
+                {opt.icon && opt.type == "fa" ? (
+                  <Icon fafa={opt.icon} width={16} />
+                ) : null}
+                {opt.icon && opt.type == null ? (
+                  <Icon src={opt.icon} width={16} />
+                ) : null}
+              </div>
+            ) : null}
+            <div className="nopt">{opt.name}</div>
+            {opt.opts ? (
+              <Icon
+                className="micon rightIcon"
+                fafa="faChevronRight"
+                width={10}
+                color="#999"
+              />
+            ) : null}
+            {opt.dot ? (
+              <Icon
+                className="micon dotIcon"
+                fafa="faCircle"
+                width={4}
+                height={4}
+              />
+            ) : null}
+            {opt.check ? (
+              <Icon
+                className="micon checkIcon"
+                fafa="faCheck"
+                width={8}
+                height={8}
+              />
+            ) : null}
+            {opt.opts ? (
+              <div
+                className="minimenu"
+                style={{
+                  minWidth: menudata.secwid,
+                }}
+              >
+                {menuobj(opt.opts)}
+              </div>
+            ) : null}
+          </div>,
+        );
       }
     });
-  });
 
-  useEffect(() => {
-    // console.log("ok")
-    var tmp = [];
-    for (var i = 0; i < sidepane.quicks.length; i++) {
-      var val = getTreeValue(setting, sidepane.quicks[i].state);
-      if (sidepane.quicks[i].name == "Theme") val = val == "dark";
-      tmp.push(val);
-    }
-
-    setPnstate(tmp);
-  }, [setting, sidepane]);
-
-  return (
-    <div
-      className="sidePane dpShad"
-      data-hide={sidepane.hide}
-      style={{ "--prefix": "PANE" }}
-    >
-      <div className="quickSettings p-5 pb-8">
-        <div className="qkCont">
-          {sidepane.quicks.map((qk, idx) => {
-            return (
-              <div key={idx} className="qkGrp">
-                <div
-                  className="qkbtn handcr prtclk"
-                  onClick={clickDispatch}
-                  data-action={qk.action}
-                  data-payload={qk.payload || qk.state}
-                  data-state={pnstates[idx]}
-                >
-                  <Icon
-                    className="quickIcon"
-                    ui={qk.ui}
-                    src={qk.src}
-                    width={14}
-                    invert={pnstates[idx] ? true : null}
-                  />
-                </div>
-                <div className="qktext">{qk.name}</div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="sliderCont">
-          <Icon className="mx-2" src="brightness" ui width={20} />
-          <input
-            id="brightnessSlider"
-            className="sliders bSlider"
-            onChange={setBrightness}
-            type="range"
-            min="10"
-            max="100"
-            defaultValue="100"
-          />
-        </div>
-        <div className="sliderCont">
-          <Icon className="mx-2" src={"audio" + tasks.audio} ui width={18} />
-          <input
-            className="sliders vSlider"
-            onChange={setVolume}
-            type="range"
-            min="0"
-            max="100"
-            defaultValue="100"
-          />
-        </div>
-      </div>
-      <div className="p-1 bottomBar">
-        <div className="px-3 battery-sidepane">
-          <Battery pct />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export const CalnWid = () => {
-  const sidepane = useSelector((state) => state.sidepane);
-  const [loaded, setLoad] = useState(false);
-
-  const [collapse, setCollapse] = useState("");
-
-  const collapseToggler = () => {
-    collapse === "" ? setCollapse("collapse") : setCollapse("");
+    return mnode;
   };
 
-  useEffect(() => {
-    if (!loaded) {
-      setLoad(true);
-      window.dycalendar.draw({
-        target: "#dycalendar",
-        type: "month",
-        dayformat: "ddd",
-        monthformat: "full",
-        prevnextbutton: "show",
-        highlighttoday: true,
-      });
-    }
-  });
-
   return (
     <div
-      className={`calnpane ${collapse} dpShad`}
-      data-hide={sidepane.calhide}
-      style={{ "--prefix": "CALN" }}
+      className="actmenu"
+      id="actmenu"
+      style={{
+        ...abpos,
+        "--prefix": "MENU",
+        width: menudata.width,
+      }}
+      data-hide={menu.hide}
+      data-left={isLeft}
     >
-      <div className="topBar pl-4 text-sm">
-        <div className="date">
-          {new Date().toLocaleDateString(undefined, {
-            weekday: "long",
-            month: "long",
-            day: "numeric",
-          })}
-        </div>
-        <div className="collapser p-2 m-4 rounded" onClick={collapseToggler}>
-          {collapse === "" ? (
-            <Icon fafa="faChevronDown" />
-          ) : (
-            <Icon fafa="faChevronUp" />
-          )}
-        </div>
-      </div>
-      <div id="dycalendar"></div>
+      {menuobj(menu.menus[menu.opts])}
     </div>
   );
 };
+
+export default ActMenu;
